ccbd404f84b2050b7eb71778304857baa634babd
xubo <xubo1395015060@qq.com>
2019-07-24 18:43:08 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,10 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+
+
+
+
 Route::get('redis', 'TestController@test');
