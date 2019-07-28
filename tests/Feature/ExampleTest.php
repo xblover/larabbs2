12ffd804f09d5219f00fb68b2b55a66896230e50
xubo <xubo1395015060@qq.com>
2019-07-28 21:11:42 +0800

    翻译队列化

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,24 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * 简单的功能测试用例
+     *
+     * @return void
+     */
+    // public function testBasicExample()
+    // {
+    //     $response = $this->withHeaders([
+    //         'X-Header'=>'LaravelAcaemy',
+    //         ])->json('POST','user',['name' => '学院君']);
+
+    //     $response
+    //         ->assertStatus(200)
+    //         ->assertJson([
+    //             'created' => true,
+    //         ]);
+    // }
+
+    
 }
