2454475aadffe84df02c1c4b6c3621e86525277f
xubo <xubo1395015060@qq.com>
2019-08-03 10:27:42 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'password', 'remember_token',
     ];
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function topics()
     {
         return $this->hasMany(Topic::class);
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 
 Route::get('redis', 'TestController@test');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
